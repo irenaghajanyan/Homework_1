acba7b424e8c3a9f5793ec9fa02311fa1b5c6822
Your Name <you@example.com>
2019-06-28 01:34:07 +0400

    first commit

---
+++ Homework1.js
@@ -0,0 +1,74 @@
+//1
+
+const a = 15 ;
+const b = 8 ;
+const sum = a+b
+const hanum = a-b
+console.log(sum * hanum )
+console.log(15>8 && 8<=a)
+
+//2
+
+const base = 12 ;
+const height  = 7 ; ;
+const area = base * height/2  ;
+console.log(area)
+
+//3
+
+  const day= "27/"
+  const month= "06/"
+const year= "2018"
+console.log(day+month+year)
+
+//4
+
+
+  const h = "11:"
+  const m= "23:"
+const s= "47"
+console.log(h+m+s)
+
+//5
+
+const name = "Alen " 
+  const surname = "Gyulazyan "
+  const age = "19"
+  console.log( name + surname + "is " + age + " years old" )
+
+  //6
+
+  
+  const a = ["hey", "hello", 73, 37, 63,[[53,89,99,"bye","goodbye"]]]
+            
+             console.log(a)
+             console.log(a[0])
+             console.log(a[1])
+             console.log(a[2])
+             console.log(a[3])
+             console.log(a[4])
+             console.log(a[5][0][0])
+             console.log(a[5][0][1])
+             console.log(a[5][0][2])
+             console.log(a[5][0][3])
+             console.log(a[5][0][4])
+
+//7
+
+
+  const type1 = "MP4" ;
+  const type2 = "FLV" ;
+  const type3 = "WMV" ;
+  const type = "MOD" ;
+  
+  if (type1 === "MP4") {
+    console.log("it is MP4 type") }
+  if (type2 === "FLV") {
+    console.log("it is FLV type") }
+  if (type3 === "WMV") {
+    consolo.log("It is FLV type") }
+  if (type4 === "MOD") {
+    consolo.log("It is MOD type") }
+
+
+
